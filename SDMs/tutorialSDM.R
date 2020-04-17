405dbcd234b950ba3b7138c6a2b32ab0fef22587
Angel Gimenez <angel.gimenez@bc3research.org>
2020-04-17 14:47:24 +0200

    Continue tutorial

--- SDMs/tutorialSDM.R
+++ SDMs/tutorialSDM.R
@@ -7,7 +7,7 @@ library(dismo)
 
 ###############
 # Retrieve occurrence records of Bombus Terrestris in Europe since 2018
-bTerrestris = gbif("bombus", "terrestris", args=c("year=2018,2020", "continent=EUROPE"))
+bTerrestris = gbif("bombus", "terrestris", args=c("year=2000,2020","country=ES"), geo=TRUE)
 
 ###############
 # Plot map
@@ -22,7 +22,8 @@ points(bTerrestris$lon, bTerrestris$lat, col='orange', pch=20, cex=0.75)
 points(bTerrestris$lon, bTerrestris$lat, col='red', cex=0.75)
 
 ###############
-# Remove duplicates
+# Remove duplicates and NA coordinates
+bTerrestris = bTerrestris[!is.na(bTerrestris$lat) & !is.na(bTerrestris$lon), ]
 dups <- duplicated(bTerrestris)
 which.min(dups==TRUE)
 bTerrestris=bTerrestris[dups==FALSE,]
@@ -64,25 +65,57 @@ points(bTerrGeo)
 # selected points in red
 points(bTerrSel, cex=1, col='red', pch='x')
 # Save selected data
-file <- paste(system.file(package="dismo"), '/ex/bombusTerrestris.csv', sep='')
+write.csv(bTerrSel, paste(system.file(package="dismo"), '/ex/bombusTerrestris.csv', sep=''))
 bTerrSel <- read.csv(file)
 
 ###############
 # Absence and background points
 # get the file names
-Belgium <- wrld_simpl[wrld_simpl$NAME=="Belgium",]
-plot(Belgium)
+Spain <- wrld_simpl[wrld_simpl$NAME=="Spain",]
+plot(Spain)
 
-a=spsample(Belgium, n=500, type="random")
-points(a, cex=0.5)
-
-# we use the first file to create a RasterLayer
-mask <- raster(Belgium)
-# select 500 random points
-# set seed to assure that the examples will always
-# have the same random sample.
+# select 100 random points. Set seed to assure that the examples will always have the same random sample.
 set.seed(1963)
-bg <- randomPoints(mask, 500 )
+bg=spsample(Spain, n=100, type="random")
+points(bg, cex=0.5)
 
+###############
+# Environmental data
+library(raster)
+wclim0 = getData('worldclim', var='bio', res=0.5, lon=-16, lat=28)
+wclim1 = getData('worldclim', var='bio', res=0.5, lon=-3.7, lat=40.41)
+wclim2 = getData('worldclim', var='bio', res=0.5, lon=1, lat=40.41)
+# annual mean temperature
+t0 = mask(wclim0$bio1_25,Spain)
+t1 = mask(wclim1$bio1_15,Spain)
+t2 = mask(wclim2$bio1_16,Spain)
+annMeanT = crop(x=merge(t0,t1,t2), y=Spain)
+# annual precipitation
+t0 = mask(wclim0$bio12_25,Spain)
+t1 = mask(wclim1$bio12_15,Spain)
+t2 = mask(wclim2$bio12_16,Spain)
+annPrecip = crop(x=merge(t0,t1,t2), y=Spain)
+# elevation
+ele <- raster(paste(system.file(package="dismo"), "/ex/elevationSpain.tiff", sep=""))
+# land cover
+lc <- raster(paste(system.file(package="dismo"), "/ex/lcSpain.tiff", sep=""))
 
+# Extract values
+selPoints = SpatialPoints(cbind(bTerrSel$lon,bTerrSel$lat))
+landcover = extract(lc, selPoints)
+bio1      = extract(annPrecip, selPoints)
+bio2      = extract(annMeanT, selPoints)
+alt       = extract(ele, selPoints)
+pb        = rep(1, length(alt))
+sdmdata1  = as.data.frame(cbind(pb, landcover, bio1, bio2, alt))
+predValues[,'lcSel'] = as.factor(predValues[,'lcSel']) # 'lcSel' is categorical variable (called a 'factor' in R ) and it is important to explicitly define it that way, so that it won't be treated like any other numerical variable.
+landcover = extract(lc, bg)
+bio1      = extract(annPrecip, bg)
+bio2      = extract(annMeanT, bg)
+alt       = extract(ele, bg)
+pb        = rep(0, length(eleBg))
+sdmdata0  = as.data.frame(cbind(pb, landcover, bio1, bio2, alt))
+sdmdata   = rbind(sdmdata0, sdmdata1)
+sdmdata   = sdmdata[!is.na(sdmdata$bio1) & !is.na(sdmdata$bio2) & !is.na(sdmdata$alt),]
+sdmdata[,'landcover'] = as.factor(sdmdata[,'landcover']) # 'landcover' is categorical variable (called a 'factor' in R ) and it is important to explicitly define it that way, so that it won't be treated like any other numerical variable.
 
