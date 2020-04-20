58efc8a50158a845ae1c8dea1346e70a979845a5
Angel Gimenez <angel.gimenez@bc3research.org>
2020-04-20 19:08:39 +0200

    GBIF: Organize files

--- GBIF/downloadWithDismo.R
+++ GBIF/downloadWithDismo.R
@@ -1,16 +1,6 @@
 
 library(dismo)
 
-countSpecies = function(species, args="", bGeo=TRUE) {
-  splitted = strsplit(species, " ")
-  genus = splitted[[1]][1]
-  sp = splitted[[1]][2]
-  count = gbif(genus, sp, args=args, geo=bGeo, download = FALSE)
-  df = as.data.frame(t(c(species,count)))
-  names(df) = names(dfCount)
-  df
-}
-
 speciesEuropeKleijn2015 = c("Bombus terrestris",
                             "Bombus lapidarius",
                             "Andrena chrysosceles",
@@ -32,13 +22,16 @@ speciesEuropeKleijn2015 = c("Bombus terrestris",
                             "Lasioglossum morio",
                             "Andrena cineraria")
 
+# Download occurrences of Bombus terrestris, from 1990-2020
 pars = c("year=1990,2020")
-dfCount = do.call(rbind, lapply(speciesEuropeKleijn2015, countSpecies, pars))
+species = speciesEuropeKleijn2015[1]
+splitted = strsplit(species, " ")
+genus = splitted[[1]][1]
+sp = splitted[[1]][2]
+bTerr = gbif(genus, sp, args=pars, geo=TRUE)
 
-# Check if species are among dominant pollinators in the OBServ dataset
-field_data_folder = "../../OBservData/Dominant_Pollinators_Data/"
-pollinators = read.csv(file = paste(field_data_folder, 'Summary_dominant_species.csv',sep=""), header = TRUE)
-pollinators = unique(pollinators$pollinator)
-dfCount["inOBServDominant"] = dfCount$species %in% pollinators
+
+
+
 
 
