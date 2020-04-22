8e93ef560df7e86adf12aee8ca1524b981d8752e
Angel Gimenez <angel.gimenez@bc3research.org>
2020-04-22 16:24:16 +0200

    Andrena Flavipes. Step 1: Join GBIF and OBServ data to get a table of locations with presence and pseudo-absence data

--- GBIF/downloadWithDismo.R
+++ GBIF/downloadWithDismo.R
@@ -1,6 +1,8 @@
 
 library(dismo)
 
+dataFolder = "C:/Users/angel.gimenez/Documents/DATA/OBServ/GBIF/"
+
 speciesEuropeKleijn2015 = c("Bombus terrestris",
                             "Bombus lapidarius",
                             "Andrena chrysosceles",
@@ -22,16 +24,64 @@ speciesEuropeKleijn2015 = c("Bombus terrestris",
                             "Lasioglossum morio",
                             "Andrena cineraria")
 
-# Download occurrences of Bombus terrestris, from 1990-2020
+# Download occurrences 
 pars = c("year=1990,2020")
-species = speciesEuropeKleijn2015[1]
+species = "Andrena nitida"
 splitted = strsplit(species, " ")
 genus = splitted[[1]][1]
 sp = splitted[[1]][2]
-bTerr = gbif(genus, sp, args=pars, geo=TRUE)
+aNitida = gbif(genus, sp, args=pars, geo=TRUE)
+write.csv(aNitida, paste(dataFolder,"andrena_nitida.csv", sep=""))
 
 
+###############
+# Plot map
+library(maptools)
+data(wrld_simpl)
+plot(wrld_simpl, xlim=c(-10,30), ylim=c(30,70), axes=TRUE, col="light yellow")
+# restore the box around the map
+box()
+# plot points
+points(aNitida$lon, aNitida$lat, col='orange', pch=20, cex=0.75)
+# plot points again to add a border, for better visibility
+points(aNitida$lon, aNitida$lat, col='red', cex=0.75)
 
+###############
+# Remove duplicates and NA coordinates
+aNitida = aNitida[!is.na(aNitida$lat) & !is.na(aNitida$lon), ]
+dups <- duplicated(aNitida)
+which.min(dups==TRUE)
+aNitida=aNitida[dups==FALSE,]
 
+###############
+# Cross-checking coordinates with country
+library(sp)
+aNitGeo = aNitida
+coordinates(aNitGeo) <- ~lon+lat
+crs(aNitGeo) <- crs(wrld_simpl)
+ovr <- over(aNitGeo, wrld_simpl)
+cntr <- ovr$NAME
+j <- which(cntr != aNitida$country)
+cbind(cntr, aNitida$country)[j,] 
 
+###############
+# Sampling bias
+# One can attempt to remove some of the sampling bias by subsampling records,
+# and this is illustrated below. However, subsampling reduces the number of
+# records, and it cannot correct the data for areas that have not been sampled at
+# all. It also suffers from the problem that locally dense records might in fact be
+# a true reflection of the relative suitable of habitat.
+r <- raster(aNitGeo)
+# set the resolution of the cells to (for example) 1 degree
+res(r) <- 1
+# expand (extend) the extent of the RasterLayer a little
+r <- extend(r, extent(r)+1)
+# sample:
+aNitSel <- gridSample(aNitGeo, r, n=1)
+# to illustrate the method and show the result
+p <- rasterToPolygons(r)
+plot(p, border='gray')
+points(aNitGeo)
+# selected points in red
+points(aNitSel, cex=1, col='red', pch='x')
 
---
+++ SDMs/AndrenaFlavipes.R
@@ -0,0 +1,162 @@
+
+###############
+rm(list=ls())
+library(dismo)
+library(dplyr)
+library(CoordinateCleaner)
+library(maptools)
+library(rgeos)
+library(sp)
+
+###############################
+# Main settings
+species = "Andrena flavipes"
+otherNames = c("Andrena.flavipes","Andrena_flavipes","Andrena_ flavipes")  # other names of the species in the OBServ database
+yrFrom  = 1990
+yrTo    = 2020
+excludeInAbsenceSelection = c("Andrena") # any pollinator name that contains any of these strings is not considered as a candidate absence point
+# excludeInAbsenceSelection = c("Andrena", "Andrena sp", "Andrena sp.", "Andrena sp. ", "Andrena sp. 4", "Andreninae", "Small Andrena sp.", "Large Andrena sp.") # any pollinator name equal to these strings is not considered as a candidate absence point
+
+# Directories
+observDir  = "C:/Users/angel.gimenez/Documents/REPOSITORIES/OBservData/Datasets_storage" 
+sdmDir     = "C:/Users/angel.gimenez/Documents/DATA/OBServ/SDMs/"
+speciesDir = paste(gsub(" ","_",species), "/", sep="")
+dataDir    = paste(sdmDir, speciesDir, sep="");  
+dir.create(file.path(sdmDir, speciesDir), showWarnings = FALSE)
+                   
+# Other settings
+csvGbif         = "gbifData.csv"
+csvObservInsect = "observInsectData.csv"
+csvObservField  = "observFieldData.csv"
+gbifReady    = TRUE
+observReady  = TRUE
+timeSpan     = paste("year=",stringr::str_c(yrFrom), sep="") %>% paste(",", sep="") %>% paste(stringr::str_c(yrTo), sep="")
+
+
+###############################
+# 1. Join GBIF and OBServ data to get a table of locations with presence and pseudo-absence data
+
+# Load GBIF data: presence-only
+if (gbifReady) {
+  dfGbif = read.csv(file = paste(dataDir,csvGbif, sep=""), header = TRUE)
+} else {
+  # Download and save GBIF data
+  pars = c(timeSpan)  # use dismo syntax for query
+  splitted = strsplit(species, " ")
+  genus = splitted[[1]][1]
+  sp = splitted[[1]][2]
+  dfGbif = gbif(genus, sp, args=pars, geo=TRUE)
+  write.csv(dfGbif, paste(dataDir, csvGbif, sep=""), row.names=FALSE)
+}
+
+# Load OBServ data: presence and pseudo-absences (OBServ locations where the species is not observed)
+if (observReady) {
+  dfInsectSampling = read.csv(file = paste(dataDir, csvObservInsect, sep=""), header = TRUE)
+  dfFieldData      = read.csv(file = paste(dataDir, csvObservField, sep=""), header = TRUE)
+} else {
+  observFiles = list.files(observDir, full.names = TRUE)
+  
+  # Insect sampling
+  indInsectSampling = lapply(observFiles, function(x){grepl("insect_sampling",x)})
+  indInsectSampling = unlist(indInsectSampling)
+  fsInsectSampling  = observFiles[indInsectSampling]
+  dfInsectSampling  = do.call(rbind, lapply(fsInsectSampling, function(file){ read.csv(file = file, header = TRUE) }))
+  dfInsectSampling  = dfInsectSampling[, names(dfInsectSampling) %in% c("study_id", "site_id", "pollinator","abundance")]
+  dfInsectSampling  = dfInsectSampling[complete.cases(dfInsectSampling),]
+  names(dfInsectSampling)[names(dfInsectSampling) == "abundance"] <- "abundance_species"
+  write.csv(dfInsectSampling, paste(dataDir, csvObservInsect, sep=""), row.names=FALSE)
+
+  # Field data
+  indFieldData = lapply(observFiles, function(x){grepl("field_level_data",x)})
+  indFieldData = unlist(indFieldData)
+  fsFieldData  = observFiles[indFieldData]
+  dfFieldData  = do.call(rbind, lapply(fsFieldData, function(file){ read.csv(file = file, header = TRUE) }))
+  dfFieldData  = dfFieldData[dfFieldData$sampling_year >= yrFrom & dfFieldData$sampling_year <= yrTo ,]
+  dfFieldData  = dfFieldData[, names(dfFieldData) %in% c("study_id", "site_id", "latitude","longitude", "abundance")]
+  dfFieldData  = dfFieldData[complete.cases(dfFieldData),]
+  names(dfFieldData)[names(dfFieldData) == "abundance"] <- "abundance_total"
+  write.csv(dfFieldData, paste(dataDir, csvObservField, sep=""), row.names=FALSE)
+  
+}
+
+# Presence data: column "presence" == 1
+dfInsectPresence = dfInsectSampling[dfInsectSampling$pollinator %in% c(species, otherNames) ,]
+dfObservPresence = merge(dfInsectPresence, dfFieldData, by=c("study_id", "site_id"))
+names(dfObservPresence)[names(dfObservPresence) == "longitude"] <- "lon"
+names(dfObservPresence)[names(dfObservPresence) == "latitude"]  <- "lat"
+dfGbifPresence   = dfGbif[c("lat","lon","country")]
+dfPresence       = plyr::rbind.fill(dfObservPresence, dfGbifPresence)
+dfPresence["presence"]  = rep(1, nrow(dfPresence))
+
+# Pseudo-absence data: column "presence" == 0
+# Exclude species that show pattern $excludeInAbsenceSelection
+i <- 1
+dfToExclude = data.frame(matrix(ncol = length(names(dfInsectSampling)), nrow = 0))
+for (i in seq(1,length(excludeInAbsenceSelection))) {
+  dfToExclude = rbind(dfToExclude, dfInsectSampling[grepl(excludeInAbsenceSelection[i], dfInsectSampling$pollinator) ,])
+  #dfToExclude = rbind(dfToExclude, dfInsectSampling[excludeInAbsenceSelection[i] == dfInsectSampling$pollinator, ])
+}
+dfInsectAbsence = anti_join(dfInsectSampling, dfToExclude)
+dfInsectAbsence = dfInsectAbsence[dfInsectAbsence$abundance_species > 0,]
+# Select those locations (study_id, site_id) not present in dfPresence
+dfInsectAbsence = anti_join(dfInsectAbsence, dfInsectPresence, by=c("study_id", "site_id"))
+# Join tables by study_id and site_id and save
+dfAbsence = merge(dfInsectAbsence, dfFieldData, by=c("study_id", "site_id"))
+names(dfAbsence)[names(dfAbsence) == "longitude"] <- "lon"
+names(dfAbsence)[names(dfAbsence) == "latitude"]  <- "lat"
+dfAbsence["presence"]  = rep(0, nrow(dfAbsence))
+# Take points within the convex hull of the presence points
+conHull   = convHull(dfPresence[c("lon","lat")])
+absPoints = SpatialPoints(cbind(dfAbsence$lon, dfAbsence$lat))
+inConHull = array()
+for(i in seq(1:length(absPoints))) {
+  inConHull[i] = gContains(conHull@polygons, absPoints[i])
+}
+dfAbsence = dfAbsence[inConHull,]
+
+# Bind presence and pseudo-absence locations
+dfLocations = rbind(dfPresence[c("presence","lon","lat")], dfAbsence[c("presence","lon","lat")])
+
+# Plot
+data(wrld_simpl)
+plot(wrld_simpl, xlim=c(-10,30), ylim=c(30,70), axes=TRUE, col="light yellow")
+# restore the box around the map
+box()
+# plot points
+points(dfPresence$lon, dfPresence$lat, col='orange', pch=20, cex=0.75)
+# plot points again to add a border, for better visibility
+points(dfPresence$lon, dfPresence$lat, col='red', cex=0.75)
+
+
+###############################
+# 2. Clean data (CoordinateCleaner + kernel density estimator (KDE))
+#rl <- clean_coordinates(dfLocations, lon = "lon", lat = "lat")
+
+
+# 3. Sampling bias. Take absence points as locations in the OBServ datasets 
+#   where the species has not bee observed (within the study region). In case this
+#   doesn't work, we will need to take random background points and subsample
+#   the presence locations.
+
+# 4. Get environmental variables at the locations. Some might be ready off
+#   the shelf (bioclim variables), but others will need to be calculated, 
+#   such as the percentage of LC types within certain radius, which in turn 
+#   can be computed according to the intertegular span of the insect.
+#   The size of the surrounding area considered to be relevant for the insect
+#   is proportional to its typical distance of flight. This distance can be 
+#   derived from the inter-tegular (IT) span and "Greenleaf et al 2007 - Bee 
+#   foraging ranges and their relationship to body size".
+
+# 5. PCA of the environmental variables
+
+# 6. Apply model: maxent or logistic regression (or both and compare)
+
+# 7. Derive landscape suitability from values given by the applied model
+
+# 8. Calculate pollination service from the landscape suitability index.
+ 
+# 9. Habitat preferences (https://bartomeuslab.com/2016/05/24/preferring-a-preference-index-ii-null-models/)
+# 
+
+
+
